80830b7ad9d29f4b633dfd420b179ff256394cef
Fluffy-Bean <michal-gdula@protonmail.com>
2023-10-16 21:54:42 +0100

    Add proper focus outline for accessibility

--- js/routeDevices.js
+++ js/routeDevices.js
@@ -30,69 +30,69 @@ function refreshDevices() {
                 let config = JSON.parse(item['config']);
 
                 let details = document.createElement('details');
-                details.style.cssText = 'padding: 0;';
-                details.className = 'tile';
-                details.dataset.device_id = item['id'];
+                    details.style.cssText = 'padding: 0;';
+                    details.className = 'tile';
+                    details.dataset.device_id = item['id'];
+                    details.open = true;
 
                 let summary = document.createElement('summary');
 
                 let h2 = document.createElement('h2');
-                h2.style.cssText = 'margin: 0; user-select: none;';
-                h2.innerText = item['name'];
+                    h2.style.cssText = 'margin: 0; user-select: none;';
+                    h2.innerText = item['name'];
 
                 let p = document.createElement('p');
-                p.style.cssText = 'margin: 0; user-select: none;';
-                p.innerText = item['location'] + ' • V' + config['version'];
+                    p.style.cssText = 'margin: 0; user-select: none;';
+                    p.innerText = item['location'] + ' • V' + config['version'];
 
                 let div = document.createElement('div');
 
                 let deviceInfo = document.createElement('p');
-                deviceInfo.style.cssText = 'margin-top: 0; user-select: none;';
-                deviceInfo.innerText = [config['address'], config['version']].join(' • ')
+                    deviceInfo.style.cssText = 'margin-top: 0; user-select: none;';
+                    deviceInfo.innerText = [config['address'], config['version']].join(' • ')
 
                 let nameDiv = document.createElement('div');
-                nameDiv.style.cssText = 'margin-bottom: 0.5rem;';
+                    nameDiv.style.cssText = 'margin-bottom: 1rem;';
 
                 let editNameLabel = document.createElement('label');
-                editNameLabel.htmlFor = 'edit-name-' + item['id'];
-                editNameLabel.innerText = 'Device name';
+                    editNameLabel.htmlFor = 'edit-name-' + item['id'];
+                    editNameLabel.innerText = 'Device name';
 
                 let editName = document.createElement('input');
-                editName.id = 'edit-name-' + item['id'];
-                editName.type = 'text';
-                editName.placeholder = 'Name';
-                editName.value = item['name'];
-                editName.style.cssText = 'margin-right: 0.5rem;';
+                    editName.id = 'edit-name-' + item['id'];
+                    editName.type = 'text';
+                    editName.placeholder = 'Name';
+                    editName.value = item['name'];
+                    editName.style.cssText = 'margin-right: 0.5rem;';
 
                 let locationDiv = document.createElement('div');
-                locationDiv.style.cssText = 'margin-bottom: 1rem;';
+                    locationDiv.style.cssText = 'margin-bottom: 1rem;';
 
                 let editLocationLabel = document.createElement('label');
-                editLocationLabel.htmlFor = 'edit-location-' + item['id'];
-                editLocationLabel.innerText = 'Device location';
+                    editLocationLabel.htmlFor = 'edit-location-' + item['id'];
+                    editLocationLabel.innerText = 'Device location';
 
                 let editLocation = document.createElement('input');
-                editLocation.id = 'edit-location-' + item['id'];
-                editLocation.type = 'text';
-                editLocation.placeholder = 'Location';
-                editLocation.value = item['location'];
+                    editLocation.id = 'edit-location-' + item['id'];
+                    editLocation.type = 'text';
+                    editLocation.placeholder = 'Location';
+                    editLocation.value = item['location'];
 
                 let saveButton = document.createElement('button');
-                saveButton.onclick = () => { updateDevice(item['id']); };
-                saveButton.style.cssText = 'margin-right: 1rem;';
-                saveButton.innerText = 'Save';
+                    saveButton.onclick = () => { updateDevice(item['id']); };
+                    saveButton.style.cssText = 'float: right;';
+                    saveButton.innerText = 'Save';
 
                 let deleteButton = document.createElement('button');
-                deleteButton.onclick = () => { deleteDevice(item['id']); };
-                deleteButton.style.cssText = 'float: right;';
-                deleteButton.className = 'button-danger';
-                deleteButton.innerText = 'Yeet';
+                    deleteButton.onclick = () => { deleteDevice(item['id']); };
+                    deleteButton.className = 'button-danger';
+                    deleteButton.innerText = 'Yeet';
 
 
                 summary.appendChild(h2);
                 summary.appendChild(p);
 
-                div.appendChild(deviceInfo);
+                // div.appendChild(deviceInfo);
 
                 nameDiv.appendChild(editNameLabel);
                 nameDiv.appendChild(editName);
@@ -102,8 +102,8 @@ function refreshDevices() {
                 locationDiv.appendChild(editLocation);
                 div.appendChild(locationDiv);
 
-                div.appendChild(saveButton);
                 div.appendChild(deleteButton);
+                div.appendChild(saveButton);
 
                 details.appendChild(summary);
                 details.appendChild(div);
